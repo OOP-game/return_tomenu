6f43c7e5c74ae59c14484c8e460378acece91052
Pun Punyawat <Pun Punyawat@LAPTOP-OINO4T9K>
2021-05-02 14:41:06 +0700

    OOP_work

---
+++ src/pyramit5/FXMLGameOverController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pyramit5;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Pun Punyawat
+ */
+public class FXMLGameOverController implements Initializable {
+
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }    
+    
+}
